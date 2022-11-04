26f400e2104d168e5f507340cd77e43493ef9a0f
Aygün Bayır <82970249+aygunbyr@users.noreply.github.com>
2022-11-04 09:44:22 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace DataReaderToObjects
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShow = new System.Windows.Forms.Button();
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(12, 12);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 0;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Location = new System.Drawing.Point(12, 41);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.RowTemplate.Height = 25;
+            this.dgvProducts.Size = new System.Drawing.Size(776, 397);
+            this.dgvProducts.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvProducts);
+            this.Controls.Add(this.btnShow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.DataGridView dgvProducts;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+
+namespace DataReaderToObjects
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            List<Product> products = null;
+            string connStr = @"Data Source=DESKTOP-5R2AJR3\SQLEXPRESS;Database=Northwind;Integrated Security=true;TrustServerCertificate=True;";
+            using(SqlConnection cn = new SqlConnection(connStr))
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("Select ProductName as Name, UnitPrice as Price, UnitsInStock as Units from Products", cn);
+                var dataReader = cmd.ExecuteReader();
+                products = GetList<Product>(dataReader);
+
+
+            }
+
+            if(products!= null)
+            {
+                dgvProducts.DataSource = products;
+            }
+
+        }
+
+        private List<T> GetList<T>(IDataReader reader)
+        {
+            List<T> list = new List<T>();
+            while(reader.Read())
+            {
+                var type = typeof(T);
+                T obj = (T)Activator.CreateInstance(type);
+                foreach(var prop in type.GetProperties())
+                {
+                    var propType = prop.PropertyType;
+                    prop.SetValue(obj, Convert.ChangeType(reader[prop.Name].ToString(), propType));
+                }
+                list.Add(obj);
+
+            }
+            return list;
+        }
+    }
+}
